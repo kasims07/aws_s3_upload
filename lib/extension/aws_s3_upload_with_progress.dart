1240ddb9a6e1cc4b22d789a2caf93491e770e2fa
Mohmadkasam <57990192+kasims07@users.noreply.github.com>
2025-01-11 18:39:30 +0530

    Update aws_s3_upload_with_progress.dart

--- lib/extension/aws_s3_upload_with_progress.dart
+++ lib/extension/aws_s3_upload_with_progress.dart
@@ -19,7 +19,7 @@ class AwsS3Progress {
     BaseOptions(
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(seconds: 30),
-      sendTimeout: const Duration(seconds: 30),
+      sendTimeout: const Duration(hours: 4),
     ),
   );
 
